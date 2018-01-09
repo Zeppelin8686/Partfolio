922b6006da89a5816dea2648f708c2c3626a3b35
Maxim Borovik <Zeppelin8686@gmail.com>
2018-01-09 23:12:17 +0400

    added parallax on sections,burger menu,remove autorizeBTN,changed HTML for burger and headers

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -73,8 +73,8 @@
 // function tabsMenu() {
 //   let tabsMenu = document.querySelector('.tabs');
 //   tabsMenu.addEventListener('click', function(e) {
-//     // e.preventDefault(); //remove basic link options
-//     let currentTab = e.target; //current tab which you click
+    // e.preventDefault(); 
+//     let currentTab = e.target; 
 //     let tab = currentTab.closest('.tabs__btn'); 
 //     let tabs = document.querySelectorAll('.tabs__btn');
 
@@ -96,23 +96,78 @@
 /** Flipper */
 ///////////////////////////////////
 
-function flipper() {
-  let login = document.querySelector('.hero__autorize-btn');
+let flipper = (function () {
+  let login = document.querySelector('.authorize-btn');
   let flip = document.querySelector('.flipper');
   let mainPageBtn = document.querySelector('.btn-main-page');
 
   login.addEventListener('click', e => {
     flip.classList.add('flipper--hover');
-    console.log(flip)
+    login.classList.add('authorize-btn--active');
   });
   mainPageBtn.addEventListener('click', e => {
     e.preventDefault();
     flip.classList.remove('flipper--hover');
+    login.classList.remove('authorize-btn--active');
   })
-}
-
+  return 
+})()
 flipper();
 
+///////////////////////////////////
+/** Hamburger-menu */
+///////////////////////////////////
+
+// let hamburgerMenu = function() {
+//   let menuBtn = document.querySelector('.hamburger-menu');
+//   let overlay = document.querySelector('.overlay');
+//   let body = document.querySelector('body');
+
+//   menuBtn.addEventListener('click', e => {
+//     overlay.classList.toggle('overlay--active');
+//     body.classList.toggle('body--active');
+//     menuBtn.classList.toggle('hamburger-menu--active');
+//   });
+//   return
+// }
+// hamburgerMenu();
+
+///////////////////////////////////
+/** Parallax */
+///////////////////////////////////
+
+// let parallax = (function() {
+//   let bg = document.querySelector('.hero__bg');
+//   let title = document.querySelector('.svg-portfolio_header-dims');
+//   let ava = document.querySelector('.owner__img');
+//   let desc = document.querySelector('.owner__desc')
+
+
+//   return {
+//     move: function(block, windowScroll, strafeAmount) {
+//       let strafe = windowScroll / -strafeAmount + '%';
+//       let transformString = 'translate3d(0, ' + strafe + ' , 0)';
+
+//       let style = block.style;
+
+//       style.transform = transformString;
+//       style.webkitTransform = transformString;
+//     },
+
+//     init: function(wScroll) {
+//       this.move(bg, wScroll, 20);
+//       this.move(title, wScroll, 10);
+//       this.move(ava, wScroll, 3);
+//       this.move(desc, wScroll, 1);
+//     }
+//   }
+// }()); 
+
+// window.onscroll = function() {
+//   var wScroll = window.pageYOffset;
+
+//   parallax.init(wScroll);
+// }
 
 /***/ })
 /******/ ]);
